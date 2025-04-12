e3330b7ffe795b6631653c40e94fbaed7497d0db
Pedro Gonçalves <pedrogoncalves.ie@gmail.com>
2025-04-12 20:36:02 -0300

    starting project

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,2 +1,18 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def connect_to_db():
+    try:
+        conn = psycopg2.connect(
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USERNAME'),
+            password=os.getenv('DB_PASSWORD'),
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT')
+        )
+        return conn
+    except Exception as e:
+        return f"Unable to connect to the database due an error: {e}"
